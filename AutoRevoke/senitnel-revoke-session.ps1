fd03f97af9860aaa3a17545a8b5af217933084e2
khjou <45777266+khjou@users.noreply.github.com>
2023-04-26 17:44:40 +0800

    Add files via upload

---
+++ AutoRevoke/senitnel-revoke-session.ps1
@@ -0,0 +1,10 @@
+$MIGuid = "<Enter your managed identity guid here>"
+$MI = Get-AzureADServicePrincipal -ObjectId $MIGuid
+
+$GraphAppId = "00000003-0000-0000-c000-000000000000"
+$PermissionName = "User.ReadWrite.All" 
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
+-ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
